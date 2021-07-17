0d048f8d335832f515ab63f79db49ab6ae5f0efb
sajeel12 <67584740+sajeel12@users.noreply.github.com>
2021-07-17 14:14:54 +0500

    Add files via upload

---
+++ javaScript/login.js
@@ -0,0 +1,63 @@
+const  myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const email = document.querySelector('#email');
+const msg = document.querySelector('#msg');
+const user = document.querySelector('#users')
+
+function access(){
+
+    location.replace("blog.html");
+}
+
+
+
+
+
+myForm.addEventListener('submit' , onSubmit)
+
+function onSubmit(e){
+    e.preventDefault();
+
+    if( nameInput.value === '' || email.value===''){
+
+        msg.innerHTML='Please fill All the fields Sajeel';
+
+        setTimeout(() => msg.remove() , 2000);
+
+    
+    }
+         
+    else if(nameInput.value==='sajeel' && email.value === 'sajeel') {
+        access();
+            
+    }   
+
+
+    else{
+
+        msg.innerHTML= "Invalid User!"
+
+        setInterval(() => msg.remove() , 2000)
+        
+
+// const li =document.createElement('li');
+
+//         li.appendChild(document.createTextNode(`Name= ${nameInput.value}: email= ${email.value}`));
+
+//         user.appendChild(li);
+
+
+
+        nameInput.value = '';
+        email.value= '';
+
+//         access();
+//         setTimeout(() => user.remove() , 1000);
+        // clearInterval(msg.remove());
+
+    }
+
+    
+
+
+}
